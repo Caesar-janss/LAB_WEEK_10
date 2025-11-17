a3ba64ac61b437d50c2a71c999eb184149d7dc6a
Jansen Caesar <caesarjansen1@gmail.com>
2025-11-17 13:15:04 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

---
+++ app/src/main/java/com/example/lab_week_10/Database/Total.kt
@@ -0,0 +1,23 @@
+package com.example.lab_week_10.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "total")
+data class Total(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Long = 1, // hanya satu record
+    @Embedded
+    val total: TotalObject
+)
+
+data class TotalObject(
+    @ColumnInfo(name = "value")
+    val value: Int,
+
+    @ColumnInfo(name = "date")
+    val date: String
+)
--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -6,18 +6,36 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
+import com.example.lab_week_10.database.TotalObject
 import com.example.lab_week_10.viewmodels.TotalViewModel
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: TotalViewModel
+    private lateinit var db: TotalDatabase
+
+    companion object {
+        const val ID: Long = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // init database
+        db = Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java,
+            "total-database"
+        ).allowMainThreadQueries().build()
+
         viewModel = ViewModelProvider(this).get(TotalViewModel::class.java)
 
+        initializeValueFromDatabase()
         prepareViewModel()
     }
 
@@ -34,5 +52,40 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    private fun initializeValueFromDatabase() {
+        val record = db.totalDao().getTotal(ID)
+
+        if (record == null) {
+            // pertama kali install
+            db.totalDao().insert(
+                Total(
+                    id = ID,
+                    total = TotalObject(
+                        value = 0,
+                        date = Date().toString()
+                    )
+                )
+            )
+            viewModel.setTotal(0)
+        } else {
+            viewModel.setTotal(record.total.value)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val curValue = viewModel.total.value ?: 0
+
+        db.totalDao().update(
+            Total(
+                id = ID,
+                total = TotalObject(
+                    value = curValue,
+                    date = Date().toString()
+                )
+            )
+        )
     }
 }
